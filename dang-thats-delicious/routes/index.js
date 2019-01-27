8f7197599d8b3c57bbb639a4e530ed009f88a989
Vindhya Raviraj <--global>
2019-01-26 23:05:08 -0500

    2018-01-26 work

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // console.log('hey!!!');
+  const me = { name: 'Vindhya', age: 100, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(me);
+  // res.send(req.query.name);
+  // res.json(req.query);
+  res.render('hello', {
+    name: 'Vindhya',
+    dog: req.query.dog
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
